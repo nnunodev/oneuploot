c2cbededbf8fbf8f3072923d36f5de25de7935e9
Nuno <nunotnunes@outlook.com>
2021-05-15 18:57:38 +0100

    set up db

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
-from flask import Flask, render_template
+import os
+import bcrypt
+from os import path
+from flask import Flask, render_template, redirect, request, url_for, session, flash
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+if path.exists("env.py"):
+    import env
+
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
 
+#route for homepage
 @app.route('/')
 def index():
-    
+
     return render_template('index.html')
+

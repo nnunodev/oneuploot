4272a93e45f9504ca0e9e64799ff625bb3ce5373
Nuno <nunotnunes@outlook.com>
2021-05-16 16:21:27 +0100

    changed pymongo pwd, when user tries to login it will compare the credentials in the db

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
 import os
+from dotenv import load_dotenv
+from pathlib import Path
 import bcrypt
-from os import path
 from flask import Flask, render_template, redirect, request, url_for, session, flash
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
-if path.exists("env.py"):
-    import env
-
 app = Flask(__name__)
+
+env_path = Path('.')/'.env'
+load_dotenv(dotenv_path=env_path)
 
 # DB settings
 
-app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
-app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-app.secret_key = os.environ.get("SECRET_KEY")
+app.config["MONGO_DBNAME"] = os.getenv("MONGO_DBNAME")
+app.config["FLASK_DEBUG"]  = os.getenv("FLASK_DEBUG")
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+mongo = PyMongo(app)
 
 # route for homepage
 
@@ -23,12 +25,32 @@ app.secret_key = os.environ.get("SECRET_KEY")
 def index():
     return render_template('index.html')
 
-# route to login page
+# route to user to login
 
 
-@app.route('/login')
+@app.route('/login', methods=['POST'])
 def login():
+    # connect users to users db.
+    users = mongo.db.users
+    # find user in the db
+    login_user = users.find_one({'name': request.form['username']})
+    # if user exists compare password to db
+    if login_user:
+        if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password']) == login_user['password']:
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+    else:
+        #if it's incorrect return error message
+        error = 'Invalid username/password'
+        return render_template('login.html', error_login=error)
+
+# route for the login page
+
+
+@app.route('/login_page')
+def login_page():
     return render_template('login.html')
+
 
 @app.route('/signup')
 def signup():

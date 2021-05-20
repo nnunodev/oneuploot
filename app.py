2d2a352345fa328c5db11abaec7577174ba6e63e
Nuno <nunotnunes@outlook.com>
2021-05-20 18:21:11 +0100

    added route for add/edit/delete games

--- app.py
+++ app.py
@@ -30,9 +30,8 @@ def index():
     else:
         return render_template('index.html')
 
+
 # route to user to login
-
-
 @app.route('/login', methods=['POST'])
 def login():
     # connect users to users db.
@@ -54,14 +53,12 @@ def login():
 
 
 # route for the login page
-
 @app.route('/login_page')
 def login_page():
     return render_template('login.html')
 
+
 # route to register user
-
-
 @app.route('/signup', methods=['POST', 'GET'])
 def signup():
     if request.method == 'POST':
@@ -84,18 +81,115 @@ def signup():
     else:
         return render_template('signup.html')
 
-#route for logout users
 
+# route for logout users
 @app.route('/logout')
 def logout():
     session.pop('username', None)
     return redirect(url_for('index'))
 
 
-#route for user profile
-
+# route for user profile
 @app.route('/<username>')
 def profile(username):
     if 'username' in session:
         user = session['username']
-        return render_template('profile.html', games=mongo.db.games.find(), genres=mongo.db.genres.find(), platforms=mongo.db.platforms.find(), user=user)
+        return render_template('profile.html', games=mongo.db.games, genres=mongo.db.genres.find(), platforms=mongo.db.platforms.find(), user=user)
+
+
+# search route for platform
+@app.route('/get_search/<search>')
+def get_search(search):
+    # check if user is logged in
+    if 'username' in session:
+        user = session['username']
+        # search for platforms
+        plat_list = list(mongo.db.games.find({"platform_name": search}))
+        return render_template("results.html", user=user, platforms=plat_list, games=mongo.db.games.find(), genres=mongo.db.genres.find())
+    else:
+        return redirect(url_for('index'))
+
+
+# search route for game genre search
+@app.route('/search_genre/<genre>')
+def search_genre(genre):
+    # checks if user is logged in
+    if 'username' in session:
+        user = session['username']
+        # search for genre name user chose in db
+        genre_list = list(mongo.db.games.find({"genre_name": genre}))
+        return render_template("results_genre.html", user=user, games=mongo.db.games.find(), genres=genre_list)
+    else:
+        return redirect(url_for('index'))
+
+
+# route for adding games page
+@app.route('/add_games')
+def add_games():
+    # checks if user is logged
+    if 'username' in session:
+        user = session['username']
+        return render_template("addgame.html", games=mongo.db.games.find(), genres=mongo.db.genres.find(), platforms=mongo.db.platforms.find(), user=user)
+    else:
+        return redirect(url_for('index'))
+
+
+# route to add the games form data in the db.
+@app.route('/insert_game', methods=['POST'])
+def insert_game():
+    user = session['username']
+    games = mongo.db.games
+    games.insert_one({
+        'user': session['username'],
+        'game_name': request.form.get('game_name'),
+        'game_image': request.form.get('game_image'),
+        'game_condition': request.form.get('game_condition'),
+        'pickup_date': request.form.get('pickup_date'),
+        'platform_name': request.form.get('platform_name'),
+        'genre_name': request.form.get('genre_name'),
+        'game_condition': request.form.get('game_condition'),
+        'game_store': request.form.get('game_store'),
+        'game_price': request.form.get('game_price'),
+        'game_value': request.form.get('game_value'),
+    })
+    return redirect(url_for('profile', username=user))
+
+
+# route to enable the user to Edit games from their list
+@app.route('/edit_game/<game_id>')
+def edit_game(game_id):
+    user = session['username']
+    game_edit = mongo.db.games.find_one({"_id": ObjectId(game_id)})
+    genres = mongo.db.genres.find()
+    platforms = mongo.db.platforms.find()
+    return render_template('editgame.html', user=user, game=game_edit, genre=genres, plat=platforms)
+
+
+# route to update games form data in the db
+@app.route('/update_game/<game_id>', methods=["POST"])
+def update_game(game_id):
+    user = session['username']
+    games = mongo.db.games
+    games.update({'_id': ObjectId(game_id)},
+                 {
+        'user': session['username'],
+        'game_name': request.form.get('game_name'),
+        'game_image': request.form.get('game_image'),
+        'game_condition': request.form.get('game_condition'),
+        'pickup_date': request.form.get('pickup_date'),
+        'platform_name': request.form.get('platform_name'),
+        'genre_name': request.form.get('genre_name'),
+        'game_condition': request.form.get('game_condition'),
+        'game_store': request.form.get('game_store'),
+        'game_price': request.form.get('game_price'),
+        'game_value': request.form.get('game_value'),
+    })
+    return redirect(url_for('profile', username=user))
+
+
+# route to enable the user to delete the game from the db
+@app.route('/delete_task/<game_id>')
+def delete_game(game_id):
+    user = session['username']
+    mongo.db.games.remove({'_id': ObjectId(game_id)})
+    return redirect(url_for('profile', username=user))
